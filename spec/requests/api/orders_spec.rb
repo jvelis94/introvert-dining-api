4df5de2aff02899cdbb23a081b6c8ef85fc28eea
Joel Velis <joelvelis94@gmail.com>
2021-09-15 14:55:43 -0400

    create orders controller with create method and test

---
+++ spec/requests/api/orders_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Api::Orders", type: :request do
+  let(:email) {"velisjoel@gmail.com"}
+
+
+  describe "POST /create" do
+    it 'initializes an order with an email' do
+      expect do 
+        post '/api/orders', params: { order: { email: email } }
+      end.to change(Order, :count).by(1)
+    end
+  end
+end
