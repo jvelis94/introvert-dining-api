19757da723737657303a3fbb538071cec34c37c1
Joel Velis <joelvelis94@gmail.com>
2021-09-16 11:15:03 -0400

    wrote test and implementation for order item quantity increment/decrement

---
+++ spec/services/update_order_item_quantity_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe UpdateOrderItemQuantity, type: :model do
+  describe '#call' do
+    let(:order) {Order.create(id: 1, email: "velisjoel@gmail.com", subtotal: 0.0)}
+    let(:food_item) {FoodItem.create(id: 1, price: 10, name: "hot dog", category: "Appetizer")}
+    let(:order_item) { OrderItem.create({price: 10.00, quantity: 1, order_id: order.id, food_item_id: food_item.id}) }
+    
+
+    it "should update order quantity up if order action === increment" do
+        updated_order_item = UpdateOrderItemQuantity.new(order, order_item, 'increment').call
+        expect(order_item.quantity).to eq(updated_order_item.quantity)
+    end
+
+    it "should update order quantity down if order action === decrement" do
+      updated_order_item = UpdateOrderItemQuantity.new(order, order_item, 'decrement').call
+      expect(order_item.quantity).to eq(updated_order_item.quantity)
+    end
+
+  end
+end
--- spec/services/update_order_subtotal_spec.rb
+++ spec/services/update_order_subtotal_spec.rb
@@ -7,8 +7,14 @@ RSpec.describe UpdateOrderSubtotal, type: :model do
     let(:new_order_item) { OrderItem.create({price: 10.00, quantity: 1, order_id: order.id, food_item_id: food_item.id}) }
     
 
-    it "should update order subtotal either up or down depending on the action" do
+    it "should update order subtotal up if action === increment" do
         updated_order = UpdateOrderSubtotal.new(order, new_order_item, 'increment').call
+
+        expect(order.subtotal).to eq(updated_order.subtotal)
+    end
+
+    it "should update order subtotal up if action === decrement" do
+        updated_order = UpdateOrderSubtotal.new(order, new_order_item, 'decrement').call
 
         expect(order.subtotal).to eq(updated_order.subtotal)
     end
